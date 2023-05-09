c6626a4260c657c7e99f3800c0d9c453766efeaf
NeoRauf <abdurraufzr@code.edu.az>
2023-05-09 01:44:31 -0700

    Done

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function getComputerChoice(min, max){
     // const number = Math.floor(Math.random() * 3) + 1;
     const number = Math.floor(Math.random() * (max-min+1)) + min;
-    return number;
+
     if (number === 1)return "Rock";
     else if (number === 2) return "Paper";
     else return "Scissors";
@@ -21,36 +21,45 @@ function game(compSelection, playerSelection){
         var playerSelection = prompt("Your turn:")
         var compSelection = getComputerChoice(1, 3)
 
-        if (compSelection==="Paper" && playerSelection==="Rock"){
+        if (compSelection==="Paper" && playerSelection.toLowerCase()==="rock"){
             computerWin++
             loseAndWin = "You Lose! Paper beats Rock"
             console.log(loseAndWin)
-        } else if (compSelection==="Paper" && playerSelection==="Scissors"){
+        } else if (compSelection==="Paper" && playerSelection.toLowerCase()==="scissors"){
             playerWin++
             loseAndWin = "You Win! Scissors beats Paper"
             console.log(loseAndWin)
-        } else if (compSelection==="Rock" && playerSelection==="Scissors"){
+        } else if (compSelection==="Paper" && playerSelection.toLowerCase()==="paper"){
+            loseAndWin = "Draw!"
+            console.log(loseAndWin)
+        } else if (compSelection==="Rock" && playerSelection.toLowerCase()==="scissors"){
             computerWin++
             loseAndWin = "You Lose! Rock beats Scissors"
             console.log(loseAndWin)
-        } else if (compSelection==="Rock" && playerSelection==="Paper"){
+        } else if (compSelection==="Rock" && playerSelection.toLowerCase()==="paper"){
             playerWin++
             loseAndWin = "You Win! Paper beats Rock"
             console.log(loseAndWin)
-        } else if (compSelection==="Scissors" && playerSelection==="Paper"){
+        } else if (compSelection==="Rock" && playerSelection.toLowerCase()==="rock"){
+            loseAndWin = "Draw!"
+            console.log(loseAndWin)
+        } else if (compSelection==="Scissors" && playerSelection.toLowerCase()==="paper"){
             computerWin++
             loseAndWin = "You Lose! Scissors beats Paper"
             console.log(loseAndWin)
-        } else if (compSelection==="Scissors" && playerSelection==="Rock"){
+        } else if (compSelection==="Scissors" && playerSelection.toLowerCase()==="rock"){
             playerWin++
             loseAndWin = "You Win! Rock beats Scissors"
+            console.log(loseAndWin)
+        } else if (compSelection==="Scissors" && playerSelection.toLowerCase()==="scissor"){
+            loseAndWin = "Draw!"
             console.log(loseAndWin)
         }
     }
 
-    if(computerWin===5){
-        return "You Lost! Computer: " + computerWin + "/nYou: " + playerWin
-    } else if(playerWin===5) {return "You Won! You: " + playerWin + " Computer: " + computerWin}
+    if(computerWin>playerWin){
+        return "You Lost! Computer: " + computerWin + "n/You: " + playerWin
+    } else if(playerWin>computerWin) {return "You Won! You: " + playerWin + " Computer: " + computerWin}
 }
 
 console.log(game(getComputerChoice(1, 3), playerSelection))
